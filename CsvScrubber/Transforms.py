9f1fc41944935319c990efe249cbec5979d6de91
Poul Hornsleth <poulh@umich.edu>
2020-01-20 16:12:32 -0500

    switched to config based transform pipeline

--- CsvScrubber/Transforms.py
+++ CsvScrubber/Transforms.py
@@ -7,49 +7,63 @@ from .Reader import Reader
 #import pandas as pd
 
 
-def create(df, transform, params):
+def create(df, transform, **params):
+    # print("--------transform create------")
+    # print(transform)
+    # print(params)
+    # print("------------------------------")
+
     t = None
     if transform == 'print':
-        t = Print(df, *params)
+        t = Print(df, **params)
 
     if transform == 'print-columns':
-        t = PrintColumnNames(df, *params)
+        t = PrintColumnNames(df, **params)
 
     if transform == 'open':
-        t = Open(df, *params)
+        t = Open(df, **params)
 
     if transform == 'save':
-        t = Save(df, *params)
+        t = Save(df, **params)
 
     if transform == 'filter':
-        t = Filter(df, *params)
+        t = Filter(df, **params)
 
     if transform == 'is-na':
-        t = IsNa(df, *params)
+        t = IsNa(df, **params)
 
     if transform == 'not-na':
-        t = NotNa(df, *params)
+        t = NotNa(df, **params)
+
+    if transform == 'contains':
+        t = Contains(df, **params)
 
     if transform == 'drop-column':
-        t = DropColumn(df, *params)
+        t = DropColumn(df, **params)
+
+    if transform == 'keep-columns':
+        t = KeepColumns(df, **params)
 
     if transform == 'camelcase':
-        t = CamelCase(df, *params)
+        t = CamelCase(df, **params)
 
     if transform == 'lower':
-        t = Lower(df, *params)
+        t = Lower(df, **params)
 
     if transform == 'date-convert':
-        t = DateConvert(df, *params)
+        t = DateConvert(df, **params)
 
     if transform == 'replace':
-        t = Replace(df, *params)
+        t = Replace(df, **params)
 
     if transform == 'strip':
-        t = Strip(df, *params)
+        t = Strip(df, **params)
 
     if transform == 'valid-email':
-        t = ValidEmail(df, *params)
+        t = ValidEmail(df, **params)
+
+    if transform == 'repair-email':
+        t = RepairEmail(df, **params)
 
     if t is None:
         raise ValueError("unsupported transform '{}'".format(transform))
@@ -58,18 +72,25 @@ def create(df, transform, params):
 
 
 class Transform:
-    def __init__(self, df, *params):
+    def __init__(self, df, **params):
+        # print("----------TRANSFORM-----------")
+
         self.df = df
-        self.params = list(params)
+        self.params = params
+        # print(self.params)
+        # print("------------------------------")
 
     def transform(self):
         return self.df
 
 
 class ColumnTransform(Transform):
-    def __init__(self, df, *params):
-        super().__init__(df, *params)
-        self.column = self.params[0]
+    def __init__(self, df, column, **params):
+        super().__init__(df, **params)
+        self.column = column
+        # print("--------COLUMN TRANS---------")
+        # print(column)
+        # print("-----------------------------")
 
 
 class CamelCase(Transform):
@@ -100,21 +121,26 @@ class IsNa(ColumnTransform):
         return self.df[self.df[self.column].isna()]
 
 
-class Filter(Transform):
+class Filter(ColumnTransform):
+    def __init__(self, df, transform_name, **params):
+        super().__init__(df, **params)
+        self.transform_name = transform_name
+
     def transform(self):
 
-        transform_name = self.params[0]
         #print("transform name: {}".format(transform_name))
-
-        t = create(self.df.copy(), transform_name, self.params[1:])
+        #transform_name = self.params.pop('transform_name')
+        t = create(self.df.copy(), self.transform_name, **self.params)
         transformed_df = t.transform()
 
-        transformed_df.set_index(t.column, inplace=True, drop=False)
+        transformed_df.set_index(self.column, inplace=True, drop=False)
 
-        indexed_df = self.df.set_index(t.column, drop=False)
+        # keep column when also makeing it an index
+        indexed_df = self.df.set_index(self.column, drop=False)
 
         results = indexed_df.drop(transformed_df.index, errors='ignore')
 
+        # remove index. don't add it back as a column since we kept it above
         return results.reset_index(drop=True)
 
 
@@ -138,8 +164,8 @@ class Strip(ColumnTransform):
 
 class Replace(ColumnTransform):
     def transform(self):
-        find = self.params[1]
-        replace = self.params[2]
+        find = self.params['find']
+        replace = self.params['replace']
         self.df[self.column] = self.df[self.column].str.replace(find,
                                                                 replace,
                                                                 regex=False)
@@ -159,21 +185,21 @@ class PrintColumnNames(Transform):
         return super().transform()
 
 
-class Open(ColumnTransform):
-    def __init__(self, df, path, *params):
+class Open(Transform):
+    def __init__(self, df, path, **params):
         #print("in open: {}".format(params))
         # the passed-in dataframe is the one from --path.  We are going to make our own
         # so we pass None up to the parent class and then set self.df via the Reader
-        super().__init__(None, *params)
+        super().__init__(None, **params)
 
         r = Reader(path)
         self.df = r.read()
-        print(self.params)
+        # print(self.params)
 
 
 class Save(Transform):
     def transform(self):
-        path = self.params[0]
+        path = self.params['path']
         w = Writer(self.df, path)
         w.write()
 
@@ -181,29 +207,73 @@ class Save(Transform):
 
 
 class ValidEmail(ColumnTransform):
+    # got this regex here: https://emailregex.com
+    Regex = """(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"""
+
+    def __init__(self, df, **params):
+        super().__init__(None, **params)
+
+        self.regex = re.compile(ValidEmail.Regex)
+
+    def valid_email(self, email):
+        return email.match(pat=self.regex)
+
     def transform(self):
-        # got this regex here: https://emailregex.com
-        pattern = """(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"""
 
-        regex = re.compile(pattern)
+        return self.df[self.df[self.column].str.match(pat=self.regex)]
 
-        return self.df[self.df[self.column].str.match(pat=regex)]
+
+# class RepairEmail(ValidEmail):
+
+#     CommonEmailDomains = ['gmail', 'aol', 'yahoo', 'hotmail']
+#     CommonEmailSuffixes = ['com', 'org', 'net', 'edu', 'gov', 'co.uk']
+
+#     def repair2(self, email):
+#         return email + email
+
+#     def repair_email(self, email):
+#         def str_find_in_list(the_string, the_list):
+#             for elem in the_list:
+#                 idx = the_string.find(elem)
+#                 if idx != -1:
+#                     return (idx, elem)
+
+#             return (-1, None)
+
+#         print(email)
+#         if super.vaild_email(email):
+#             print("valid email {}".format(email))
+#             return email
+
+#         domain_idx, _domain = str_find_in_list(email,
+#                                                RepairEmail.CommonEmailDomains)
+#         suff_idx, _suffix = str_find_in_list(email,
+#                                              RepairEmail.CommonEmailSuffixes)
+
+#         if domain_idx != -1:
+#             if email.index('@') is None:
+#                 print("missing @: {}".format(email))
+#                 email = email[:domain_idx] + '@' + email[domain_idx:]
+#             elif suff_idx == -1:
+#                 email = email + '.com'
+
+#         return email
+
+#     def transform(self):
+#         self.df[self.column] = self.df[self.column].apply(self.repair2)
+
+#         return self.df
 
 
 class DateConvert(ColumnTransform):
-    def __init__(self, df, *params):
-        super().__init__(df, *params)
-        self.strptime_format = self.params[1]
-        self.strftime_format = self.params[2]
-
     def date_string_to_ymd(self, date_str):
         # print("converting '{}' using '{}' and '{}'".format(
         #     date_str, self.strptime_format, self.strftime_format))
         # clubhouse mongo format: "%Y-%m-%dT%H:%M:%S.%f%z"
 
-        dt = datetime.strptime(date_str, self.strptime_format)
+        dt = datetime.strptime(date_str, self.params['strptime'])
         local_dt = dt.replace(tzinfo=timezone.utc).astimezone(tz=None)
-        local_date_str = local_dt.strftime(self.strftime_format)
+        local_date_str = local_dt.strftime(self.params['strftime'])
         return local_date_str
 
     def transform(self):
@@ -216,3 +286,17 @@ class DateConvert(ColumnTransform):
 class DropColumn(ColumnTransform):
     def transform(self):
         return self.df.drop(self.column, axis=1)
+
+
+class KeepColumns(ColumnTransform):
+    def transform(self):
+
+        return self.df[self.column]
+
+
+class Contains(ColumnTransform):
+    def transform(self):
+
+        # what ever params are allowed here
+        # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Series.str.contains.html
+        return self.df[self.df[self.column].str.contains(**self.params)]
--- csv_scrubber.py
+++ csv_scrubber.py
@@ -9,49 +9,20 @@ import numpy as np
 import logging
 import CsvScrubber
 from CsvScrubber import Transforms
+import json
 
 
 def parse_args():
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--path", help="path csv,tsv,xls,or xlsx file")
-    parser.add_argument('-t',
-                        '--transform',
-                        dest='transforms',
-                        action='append',
-                        help='<Required> Set flag',
-                        required=True)
-
-    parser.add_argument("--clubhouse-path",
-                        help="path to Clubhouse converted CSV")
-
-    parser.add_argument("--out", help="path of output CSV")
+    parser.add_argument('-v', '--verbose', action='store_true')
+    parser.add_argument('-c', '--config')
+    parser.add_argument('-p', '--print', action='store_true')
+    parser.add_argument('-s', '--save')
     args = parser.parse_args()
 
     return args
-
-
-def date_string_to_ymd(str):
-    # print(str)
-
-    dt = datetime.strptime(str, "%a %b %d %Y %H:%M:%S %Z%z (UTC)")
-    local_dt = dt.replace(tzinfo=timezone.utc).astimezone(tz=None)
-
-    # dt.tzinfo =
-    # print(dt.tzinfo)
-    # print(dt.strftime("%Y-%m-%d %H:%M:%S"))
-    # print(local_dt.strftime("%Y-%m-%d %H:%M:%S"))
-
-    local_date_str = local_dt.strftime("%Y-%m-%d %H:%M:%S")
-    logging.info(local_date_str)
-    return local_date_str
-
-
-def camel_case(word):
-    parts = str.split(word.title())
-    parts[0] = parts[0].lower()
-    rval = ''.join(parts)
-    return rval
 
 
 def tele(t):
@@ -68,63 +39,6 @@ def tele(t):
     except:
         logging.warning("could not convert '{}'".format(t))
     return t
-
-
-def read_clubhouse_mongo(path):
-    clubhouse = pd.read_csv(path, index_col='email')
-
-    clubhouse = clubhouse[[
-        'firstName', 'lastName', 'homePhone', 'workPhone', 'otherPhone',
-        'mobilePhone'
-    ]]
-
-    return clubhouse.rename(camel_case)
-
-
-def read_clubhouse_download(path):
-    clubhouse = pd.read_csv(path)
-    clubhouse = clubhouse.rename(columns=camel_case)
-
-    for key in ['created', 'modified', 'checkinDate']:
-        clubhouse[key] = clubhouse[key].apply(date_string_to_ymd)
-
-    for phone in ['homePhone', 'workPhone', 'mobilePhone', 'otherPhone']:
-        clubhouse[phone] = clubhouse[phone].apply(tele)
-
-    clubhouse['email'] = clubhouse['email'].apply(lambda e: e.lower())
-
-    clubhouse.set_index('email', inplace=True)
-
-    return clubhouse
-
-
-def read_mp_repaired_xlsx(path):
-    members = pd.read_excel(path)
-    members = members.rename(columns=camel_case)
-
-
-def read_member_planet_converted_csv(path):
-    members = pd.read_csv(path)
-    members = members.rename(columns=camel_case)
-
-    members.dropna(subset=['email'], inplace=True)
-
-    # lowercase email addresses. easier to merge
-    members['email'] = members['email'].apply(lambda e: e.lower())
-    print(members)
-
-    members.set_index('email', inplace=True)
-    for col in ['initiatedDate', 'paidThroughDate']:
-        members[col] = members[col].apply(
-            lambda s: datetime.strptime(s, "%m/%d/%y"))
-
-    # members = members[members['Home Phone'] != '[Restricted]']
-
-    return members
-
-
-def a_not_in_b(df_a, df_b):
-    return df_a.drop(df_b.index, errors='ignore')
 
 
 def duplicate_emails(df):
@@ -157,73 +71,27 @@ def spaces_around_value(df, col_name):
 
 def main():
     args = parse_args()
-
-    member_planet = read_member_planet_converted_csv(args.mp_path)
-    clubhouse = read_clubhouse_download(args.clubhouse_path)
-
-    new_invitees = a_not_in_b(clubhouse, member_planet)
-
-    new_invitees.to_csv(args.out)
-
-    exit()
-    # print(member_planet[
-    #     member_planet['firstName'].str.strip() != member_planet['firstName']])
-    # exit()
-    # member_planet = member_planet[member_planet['firstName'] == "Poul"]
-    # print(member_planet)
-    # exit()
-    # member_planet = member_planet[['firstName', 'lastName', 'paidThroughDate']]
-    # member_planet.sort_values(by=['lastName'], inplace=True)
-
-    # member_planet.to_excel("cathy_paid_through_list_2019-11-10.xlsx")
-
-    # clubhouse = read_clubhouse_mongo('updated_clubhouse.csv')
-    # print(a_not_in_b(member_planet, clubhouse))
-    # print(a_not_in_b(clubhouse, member_planet))
-
-
-def main2():
-    args = parse_args()
-    mp = read_member_planet_converted_csv('member_planet.2019-12-02.csv')
-    cols = [
-        'firstName', 'lastName', 'initiatedDate', 'status', 'memberType',
-        'level', 'paidThroughDate', 'memberRating', 'homePhone', 'mobilePhone',
-        'workPhone', 'otherPhone'
-    ]
-    print(mp[cols])
-
-    mp['level'].fillna('Unpaid', inplace=True)
-    mp['joinedMonth'] = mp['initiatedDate'].apply(lambda d: d.strftime("%Y%m"))
-    mp['paidMonth'] = mp['paidThroughDate'].apply(lambda d: d.strftime("%Y%m"))
-
-    #print(mp.groupby(['level', 'paidMonth'], as_index=False).count())
-
-    paidThroughStats = pd.pivot_table(mp,
-                                      values='mpId',
-                                      index=['level'],
-                                      columns=['paidMonth'],
-                                      aggfunc='count').fillna('--')
-
-    print(mp[(mp['paidMonth'] != '202001')
-             & (mp['paidMonth'] != '200211')
-             & (mp['level'] != 'Lifetime')][cols])
-
-    print(paidThroughStats)
-
-
-def main4():
-    args = parse_args()
-
+    if args.verbose == True:
+        print(args)
     r = CsvScrubber.Reader(args.path)
     df = r.read()
 
-    for transform in args.transforms:
-        # this will put first value into transform and rest in a list (params)
-        transform, *params = transform.split('::')
+    if args.config:
+        with open(args.config) as json_file:
+            data = json.load(json_file)
+            for transform in data['transforms']:
+                transform_name = transform.pop('transform')
+                transform = Transforms.create(df, transform_name, **transform)
+                df = transform.transform()
 
-        transform = Transforms.create(df, transform, params)
+    if args.print:
+        transform = Transforms.create(df, 'print')
+        df = transform.transform()
 
+    if args.save:
+        params = {'path': args.save}
+        transform = Transforms.create(df, 'save', **params)
         df = transform.transform()
 
 
-main4()
+main()
